046686e0dabd37e7439c2269ce5277b5c6f440d9
Pravallikavittanala <pravallika.aids24@cmrit.ac.in>
2026-03-17 22:31:22 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -12,14 +12,22 @@ def home():
 
 @app.route("/upload", methods=["POST"])
 def upload():
-    file = request.files["image"]
+    if 'image' not in request.files:
+        return "❌ No file part"
+
+    file = request.files['image']
+
+    if file.filename == '':
+        return "❌ No selected file"
 
     if file:
+        os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
         filepath = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
         file.save(filepath)
-        return "Image uploaded successfully!"
 
-    return "Upload failed"
+        return render_template("index.html", result=result, filename=file.filename)
+
+    return "❌ Upload failed"
 
 if __name__ == "__main__":
     app.run(debug=True)
